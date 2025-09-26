76db3a455d9f0e668afe207b5812485c0f677cf8
viveknaidu007 <vivek.pavankalyan@gmail.com>
2025-09-26 20:06:05 +0530

    update

---
+++ .history/HMS/hotel/models_20240702123916.py
@@ -0,0 +1,34 @@
+from django.db import models
+from django.conf import *
+
+# Create your models here.
+
+class Room(models.Model):
+    ROOM_CATEGORIES = (
+        ("YAC","AC"),
+        ("NAC","NON-AC"),
+        ("DEL","DELUXE"),
+        ("KIN","KING"),
+        ("QUE","QUEEN"),
+    )
+
+    number=models.IntegerField()
+    category=models.CharField(max_length=3,choices=ROOM_CATEGORIES)
+    beds=models.IntegerField()
+    capacity=models.IntegerField()
+
+
+    def __str__(self):
+        return f"{self.number}, {self.category} with {self.beds} beds for {self.capacity} people"
+    
+
+class Booking(models.Model):
+    user=models.ForeignKey(settings.Auth_USER_MODEL, on_delete=models.CASCADE)
+    room=models.ForeignKey(Room, on_delete=models.CASCADE)
+    check_in=models.DateTimeField()
+    check_out=models.DateTimeField()
+
+    def _str__(self):
+        return f"{self.user}"
+
+    
--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # repo-126-Room_availability-django
 here im perfoming with hotel management system with backend
+
+
+django-admin statproject HMS
+
+CD HMS
+
+django-admin startapp hotel
